a406a3d31434070c4f17abdca39dc73d200fb2f0
Craig Smith <craigm.smith@au.westfield.com>
2014-07-31 14:24:55 +1000

    Added description

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140730234945) do
+ActiveRecord::Schema.define(version: 20140731041941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20140730234945) do
     t.datetime "updated_at"
     t.string   "kind"
     t.string   "slug"
+    t.text     "description"
   end
 
 end
