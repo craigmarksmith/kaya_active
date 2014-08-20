c10c9141f4ece3f3702c7c93091837b3ab43b3ce
Craig Smith <craigmarksmith@Craig-Smiths-MacBook-Pro.local>
2014-08-20 14:55:14 +1000

    Added HTTP caching

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
 
   # don't add this just yet, I want to keep things refreshing since we have no traffic and changes are frequent.
-  # before_action do
-  #   expires_in 1.hour, public: true unless Rails.env.development? || Rails.env.test?
-  # end
+  before_action do
+    expires_in 1.hour, public: true unless Rails.env.development? || Rails.env.test?
+  end
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
