8bc49f052129952249e88d024754e931237183f1
Craig Smith <craigmarksmith@Craig-Smiths-MacBook-Pro.local>
2014-08-20 19:31:08 +1000

    Catch 404 and render some text

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, with: :handle_404
+
+  def handle_404
+    render text: 'Not found', status: :not_found
+  end
 end
