1d235f989f0b53555dbcddd4556df49d79892376
Craig Smith <craigmarksmith@Craig-Smiths-MacBook-Pro.local>
2014-08-20 16:32:29 +1000

    Landing page with category links

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,2 +1,7 @@
 class LandingController < ApplicationController
+  def index
+    @leggings_product = Product.find_by_slug('leo-lover-full-length')
+    @body_suit_product = Product.find_by_slug('caleidoscope-body-suit-1')
+    @belt_product = Product.find_by_slug('active-black-belt-belt')
+  end
 end
