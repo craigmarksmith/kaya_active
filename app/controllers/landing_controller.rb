95999edf645b9750abb6a6ed58098037d22d45b5
Craig Smith <craigmarksmith@Craig-Smiths-MacBook-Pro.local>
2014-08-24 20:59:37 +1000

    Added production products to homepage

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,7 +1,7 @@
 class LandingController < ApplicationController
   def index
-    @leggings_product = Product.find_by_slug('leo-lover-full-length')
-    @body_suit_product = Product.find_by_slug('caleidoscope-body-suit-1')
-    @belt_product = Product.find_by_slug('active-black-belt-belt')
+    @leggings_product = Product.find_by_slug('leo-lover-long-leggings')
+    @body_suit_product = Product.find_by_slug('funky-zebra-body-suit')
+    @belt_product = Product.find_by_slug('active-running-belt-black')
   end
 end
