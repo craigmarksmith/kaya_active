7ca73326645d226b6df9bdcd29b3f18aadc9eaa9
Craig Smith <craigm.smith@au.westfield.com>
2014-11-03 20:33:15 +1100

    deployable version of the new homepage

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,7 +1,2 @@
 class LandingController < ApplicationController
-  def index
-    @leggings_product = Product.find_by_slug('leo-lover-long-leggings')
-    @body_suit_product = Product.find_by_slug('funky-zebra-body-suit')
-    @belt_product = Product.find_by_slug('active-running-belt-black')
-  end
 end
