6d406f6f1f856fa803ff48e6d555fd00d625e6e1
Craig Smith <craigm.smith@au.westfield.com>
2014-12-02 17:13:02 +1100

    New navigation.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     current_page? action: :index, controller: :landing
   end
 
+  def category_page?
+    current_page? action: :index, controller: :products
+  end
+
   def form_auth_field
     tag(:input, :type => "hidden", :name => request_forgery_protection_token.to_s, :value => form_authenticity_token)
   end
