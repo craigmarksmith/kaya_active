2b8f07fb6848236536aa4e3d1c3f4f937c24b354
Craig Smith <craigm.smith@au.westfield.com>
2014-12-22 18:54:41 +1100

    Reduced image qualities so the site speeds up a bit.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,19 @@ module ApplicationHelper
   def form_auth_field
     tag(:input, :type => "hidden", :name => request_forgery_protection_token.to_s, :value => form_authenticity_token)
   end
+
+  def cl_image_tag(image_id, options = {})
+
+    if is_mobile_request?
+      mobile_quality = options.delete(:mobile_quality)
+      mobile_width = options.delete(:mobile_width)
+      mobile_height = options.delete(:mobile_height)
+
+      options.merge!({quality: mobile_quality}) if mobile_quality
+      options.merge!({width: mobile_width}) if mobile_width
+      options.merge!({height: mobile_height}) if mobile_height
+    end
+
+    super image_id, options
+  end
 end
