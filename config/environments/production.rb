58bc8154011c005e5724ef4b47d0edc62662ce71
Craig Smith <craigm.smith@au.westfield.com>
2014-12-28 10:51:38 +1100

    Static assets should live for a week

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = false
-  config.static_cache_control = 'public, max-age=3600'
+  config.static_cache_control = 'public, max-age=604800'
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
