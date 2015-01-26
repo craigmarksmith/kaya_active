c1713c48e3ee64dc8b4f827b2cd22464c6ef06c1
Craig Smith <craigm.smith@au.westfield.com>
2015-01-26 17:44:03 +1100

    New blog

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,4 +6,3 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w( purchase.js accordion.js )
-Rails.application.config.assets.paths << "#{Rails.root}/app/assets/videos"
