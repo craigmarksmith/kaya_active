dd7751e25d58cabe2b784e089276d7c718cd1c5a
Craig Smith <craigm.smith@au.westfield.com>
2015-03-28 17:52:24 +1100

    All the current posts should be published

--- db/migrate/20150328063911_add_publish_to_blog_post.rb
+++ db/migrate/20150328063911_add_publish_to_blog_post.rb
@@ -1,5 +1,6 @@
 class AddPublishToBlogPost < ActiveRecord::Migration
   def change
     add_column :blog_posts, :publish, :boolean, default: false
+    BlogPost.all.each{|p| p.update_attribute(:publish, true)}
   end
 end
