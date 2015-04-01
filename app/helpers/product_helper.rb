9046cfe6d1e92e34e0806464380ff66495eb0a38
Craig Smith <craigm.smith@au.westfield.com>
2015-04-01 22:03:38 +1100

    Added the mobile categories menu

--- app/helpers/product_helper.rb
+++ app/helpers/product_helper.rb
@@ -10,4 +10,33 @@ module ProductHelper
     path = category_id ? category_products_path(category:category_id) : products_path
     link_to category_name, path
   end
+
+  def categories
+    [
+      {
+        name: 'All',
+        path: products_path
+      },
+      {
+        name: "Leggings",
+        path: category_products_path(category:'leggings')
+      },
+      {
+        name: "Tops",
+        path: category_products_path(category:'tops')
+      },
+      {
+        name: "Yoga Mats",
+        path: category_products_path(category:'yoga-mats')
+      },
+      {
+        name: "Bodysuits",
+        path: category_products_path(category:'bodysuits')
+      },
+      {
+        name: "Belts",
+        path: category_products_path(category:'belts')
+      }
+    ]
+  end
 end
