98e61c1f8d8f80daefbb1affeeb09664de2e4268
Craig Smith <craigm.smith@au.westfield.com>
2015-11-24 18:21:09 +1100

    Added size to basket line item

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -2,12 +2,11 @@ class BasketsController < ApplicationController
 
   def show
     @basket = Basket.new(session)
-    # @basket.add(params[:product], nil)
   end
 
   def create
     @basket = Basket.new(session)
-    @basket.add(params[:product], nil)
+    @basket.add(params[:product], params[:size])
     redirect_to basket_path
   end
 
