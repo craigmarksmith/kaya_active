6f6441a4149ef9c81af6a2fa852a74fd9e9b360b
Craig Smith <craigm.smith@au.westfield.com>
2015-12-30 20:17:04 +1100

    Added new html confirmation email

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -2,6 +2,9 @@ class PurchaseMailer < ActionMailer::Base
   default from: "no-reply@kayaactive.com.au"
 
   def confirmation(purchase)
+
+    attachments.inline['image.jpg'] = File.read(File.join(Rails.root,'/public/email_image.png'))
+
     @purchase = purchase
     mail(to: @purchase.email_address, subject: 'Your order with KayaActive.com.au')
   end
